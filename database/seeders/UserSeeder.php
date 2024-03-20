603e55ad42cb728cb18bb4e90ac4e0aa394a8ad5
Oliver O'Keeffe <oliverokeeffe@Olivers-Air.station>
2024-03-20 18:48:09 +0000

    Got the user, roles and user_roles table to seed without errors

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Role; 
+use App\Models\User;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +14,27 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $role_admin = Role::where('name', 'admin')->first();
+        $role_user = Role::where('name', 'user')->first();
+
+        $admin = new User;
+        $admin->name = "Oliver O'Keeffe";
+        $admin->email = "admin@caexample.com";
+        $admin->password = "secret123";
+        $admin->save();
+
+        //attach admin role to the user created above
+        $admin->roles()->attach($role_admin);
+
+        $user = new User;
+        $user->name = "John Jones";
+        $user->email = "user@caexample.com";
+        $user->password = "secret123";
+        $user->save();
+
+        $user->roles()->attach($role_user);
+
+
+
     }
 }
