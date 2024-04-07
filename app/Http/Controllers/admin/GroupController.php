df5160a8cadbac9352424760b09b60865f10134c
Oliver O'Keeffe <oliverokeeffe@OLIVERS-AIR.station>
2024-04-07 14:49:39 +0100

    Can now see all of the Groups on the group index page

--- app/Http/Controllers/admin/GroupController.php
+++ app/Http/Controllers/admin/GroupController.php
@@ -16,7 +16,7 @@ class GroupController extends Controller
      */
     public function index()
     {
-        $groups = Group::paginate(10);
+        $groups = Group::all();
         return view('admin.groups.index')->with('groups', $groups);
     }
 
