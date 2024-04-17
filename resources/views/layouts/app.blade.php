cbd7b7bb83170447ce1e8370e6fe194c9668f0e8
Oliver O'Keeffe <oliverokeeffe@OLIVERS-AIR.station>
2024-04-17 20:40:10 +0100

    Got the background to be blue when logged in, trying to change the landing page to blue

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,7 @@
     <body class="font-sans antialiased">
             <!-- Leaflet JavaScript -->
     <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+        <div class="min-h-screen bg-blue-500">
             @include('layouts.navigation')
 
             <!-- Page Heading -->
