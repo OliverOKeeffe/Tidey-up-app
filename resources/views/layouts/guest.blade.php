9d9828631bbef6d05903f24afe7e846d20b20458
Oliver O'Keeffe <oliverokeeffe@OLIVERS-AIR.station>
2024-04-18 19:18:00 +0100

    Did more styling on the welcome page and also the login and register pages

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,10 +15,10 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
+        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-blue-300 dark:bg-gray-900">
             <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                <a href="{{ url('/')}}">
+                    <img src="{{ asset('logo/beach-svgrepo-com.svg') }}" alt="Logo" style="width: 64px; height: 64px;">
                 </a>
             </div>
 
